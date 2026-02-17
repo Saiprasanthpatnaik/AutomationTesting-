af258b487bbc3a05033610c97715443d2bc962d9
Venkateswarlu <venkatesh78199@gmail.com>
2026-02-17 12:57:33 +0530

    Day 1 - Launch Chrome Browser

---
+++ src/main/java/com/sai/Day1LaunchBrowser/App.java
@@ -0,0 +1,10 @@
+package com.sai.Day1LaunchBrowser;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/sai/Day1LaunchBrowser/LaunchChrome.java
@@ -0,0 +1,16 @@
+package com.sai.Day1LaunchBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        driver.quit();
+    }
+}
